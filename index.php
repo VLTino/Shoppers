8ac64a8d708d6eb200d9c7708294bdab7e19016e
VLTino <valent14206@gmail.com>
2023-06-26 17:06:06 +0700

    well

--- index.php
+++ index.php
@@ -1,42 +1,45 @@
-<?php 
-require 'admins/functions.php'; 
+<?php
+require 'admins/functions.php';
 
 $imgh = query("SELECT * FROM `imgheader` WHERE `id`=1");
 $header = query("SELECT * FROM `header` WHERE `id`=1");
 $benefit = query("SELECT * FROM `benefit`");
 $category = query("SELECT * FROM `category`");
+$newprd = query("SELECT * FROM `product` ORDER BY id DESC LIMIT 5");
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <title>Shoppers &mdash; Colorlib e-Commerce Template</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet" >
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<head>
+  <title>Shoppers &mdash; Colorlib e-Commerce Template</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Mukta:300,400,700"> 
-    <link rel="stylesheet" href="fonts/icomoon/style.css">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+  <link
+    href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+    rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/magnific-popup.css">
-    <link rel="stylesheet" href="css/jquery-ui.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Mukta:300,400,700">
+  <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/magnific-popup.css">
+  <link rel="stylesheet" href="css/jquery-ui.css">
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+  <link rel="stylesheet" href="css/owl.theme.default.min.css">
 
 
-    <link rel="stylesheet" href="css/aos.css">
+  <link rel="stylesheet" href="css/aos.css">
 
-    <link rel="stylesheet" href="css/style.css">
-    
-  </head>
-  <body>
-  
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
   <div class="site-wrap">
     <header class="site-navbar" role="banner">
       <div class="site-navbar-top">
@@ -66,15 +69,16 @@ $category = query("SELECT * FROM `category`");
                       <span class="icon icon-shopping_cart"></span>
                       <span class="count">2</span>
                     </a>
-                  </li> 
-                  <li class="d-inline-block d-md-none ml-md-0"><a href="#" class="site-menu-toggle js-menu-toggle"><span class="icon-menu"></span></a></li>
+                  </li>
+                  <li class="d-inline-block d-md-none ml-md-0"><a href="#" class="site-menu-toggle js-menu-toggle"><span
+                        class="icon-menu"></span></a></li>
                 </ul>
-              </div> 
+              </div>
             </div>
 
           </div>
         </div>
-      </div> 
+      </div>
       <nav class="site-navigation text-right text-md-center" role="navigation">
         <div class="container">
           <ul class="site-menu js-clone-nav d-none d-md-block">
@@ -111,18 +115,22 @@ $category = query("SELECT * FROM `category`");
       </nav>
     </header>
 
-    <?php foreach ($imgh as $img) : ?>
-    <div class="site-blocks-cover" style="background-image: url(images/<?= $img["gambar"]; ?>);" data-aos="fade">
-    <?php endforeach; ?>
+    <?php foreach ($imgh as $img): ?>
+      <div class="site-blocks-cover" style="background-image: url(images/<?= $img["gambar"]; ?>);" data-aos="fade">
+      <?php endforeach; ?>
       <div class="container">
         <div class="row align-items-start align-items-md-center justify-content-end">
           <div class="col-md-5 text-center text-md-left pt-5 pt-md-0">
-            <?php foreach ($header as $hdr):?>
-            <h1 class="mb-2"><?= $hdr["header"]; ?></h1>
-            <div class="intro-text text-center text-md-left">
-              <p class="mb-4"><?= $hdr["teks"]; ?></p>
-              <p>
-            <?php endforeach; ?>    
+            <?php foreach ($header as $hdr): ?>
+              <h1 class="mb-2">
+                <?= $hdr["header"]; ?>
+              </h1>
+              <div class="intro-text text-center text-md-left">
+                <p class="mb-4">
+                  <?= $hdr["teks"]; ?>
+                </p>
+                <p>
+                <?php endforeach; ?>
                 <a href="#" class="btn btn-sm btn-primary">Shop Now</a>
               </p>
             </div>
@@ -134,17 +142,21 @@ $category = query("SELECT * FROM `category`");
     <div class="site-section site-section-sm site-blocks-1">
       <div class="container">
         <div class="row">
-          <?php foreach ($benefit as $bnf):?>
-          <div class="col-md-6 col-lg-4 d-lg-flex mb-4 mb-lg-0 pl-4" data-aos="fade-up" data-aos-delay="">
-            <div class="icon mr-4 align-self-start">
-              <span class="<?= $bnf["icon"];?>"></span>
+          <?php foreach ($benefit as $bnf): ?>
+            <div class="col-md-6 col-lg-4 d-lg-flex mb-4 mb-lg-0 pl-4" data-aos="fade-up" data-aos-delay="">
+              <div class="icon mr-4 align-self-start">
+                <span class="<?= $bnf["icon"]; ?>"></span>
+              </div>
+              <div class="text">
+                <h2 class="text-uppercase">
+                  <?= $bnf["header"]; ?>
+                </h2>
+                <p>
+                  <?= $bnf["teks"]; ?>
+                </p>
+              </div>
             </div>
-            <div class="text">
-              <h2 class="text-uppercase"><?= $bnf["header"];?></h2>
-              <p><?= $bnf["teks"];?></p>
-            </div>
-          </div>
-         <?php endforeach; ?>
+          <?php endforeach; ?>
         </div>
       </div>
     </div>
@@ -152,20 +164,24 @@ $category = query("SELECT * FROM `category`");
     <div class="site-section site-blocks-2">
       <div class="container">
         <div class="row">
-          <?php foreach ($category as $ctg):?>
-          <div class="col-sm-6 col-md-6 col-lg-4 mb-4 mb-lg-0" data-aos="fade" data-aos-delay="">
-            <a class="block-2-item" href="<?= $ctg["link"]; ?>">
-              <figure class="image">
-                <img src="images/<?= $ctg["gambar"]; ?>" alt="" class="img-fluid">
-              </figure>
-              <div class="text">
-                <span class="text-uppercase"><?= $ctg["teks"]; ?></span>
-                <h3><?= $ctg["category"]; ?></h3>
-              </div>
-            </a>
-          </div>
+          <?php foreach ($category as $ctg): ?>
+            <div class="col-sm-6 col-md-6 col-lg-4 mb-4 mb-lg-0" data-aos="fade" data-aos-delay="">
+              <a class="block-2-item" href="<?= $ctg["link"]; ?>">
+                <figure class="image">
+                  <img src="images/<?= $ctg["gambar"]; ?>" alt="" class="img-fluid">
+                </figure>
+                <div class="text">
+                  <span class="text-uppercase">
+                    <?= $ctg["teks"]; ?>
+                  </span>
+                  <h3>
+                    <?= $ctg["category"]; ?>
+                  </h3>
+                </div>
+              </a>
+            </div>
           <?php endforeach; ?>
-          
+
         </div>
       </div>
     </div>
@@ -180,66 +196,30 @@ $category = query("SELECT * FROM `category`");
         <div class="row">
           <div class="col-md-12">
             <div class="nonloop-block-3 owl-carousel">
-              <div class="item">
-                <div class="block-4 text-center">
-                  <figure class="block-4-image">
-                    <img src="images/cloth_1.jpg" alt="Image placeholder" class="img-fluid">
-                  </figure>
-                  <div class="block-4-text p-4">
-                    <h3><a href="#">Tank Top</a></h3>
-                    <p class="mb-0">Finding perfect t-shirt</p>
-                    <p class="text-primary font-weight-bold">$50</p>
+              <?php foreach ($newprd as $prd): ?>
+                <div class="item">
+                  <div class="block-4 text-center">
+                    <figure class="block-4-image">
+                      <img src="images/<?= $prd["gambar"]; ?>" alt="<?= $prd["name"]; ?>" class="img-fluid">
+                    </figure>
+                    <div class="block-4-text p-4">
+                      <h3><a href="#">
+                          <?= $prd["name"]; ?>
+                        </a></h3>
+                      <p class="mb-0">
+                        <?= $prd["short"]; ?>
+                      </p>
+                      <p class="text-primary font-weight-bold">
+                        <?php $priceFromDatabase = $prd["price"];
+                        $formattedPrice = "Rp " . number_format($priceFromDatabase, 0, ',', '.');
+                        echo $formattedPrice; ?>
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div class="item">
-                <div class="block-4 text-center">
-                  <figure class="block-4-image">
-                    <img src="images/shoe_1.jpg" alt="Image placeholder" class="img-fluid">
-                  </figure>
-                  <div class="block-4-text p-4">
-                    <h3><a href="#">Corater</a></h3>
-                    <p class="mb-0">Finding perfect products</p>
-                    <p class="text-primary font-weight-bold">$50</p>
-                  </div>
-                </div>
-              </div>
-              <div class="item">
-                <div class="block-4 text-center">
-                  <figure class="block-4-image">
-                    <img src="images/cloth_2.jpg" alt="Image placeholder" class="img-fluid">
-                  </figure>
-                  <div class="block-4-text p-4">
-                    <h3><a href="#">Polo Shirt</a></h3>
-                    <p class="mb-0">Finding perfect products</p>
-                    <p class="text-primary font-weight-bold">$50</p>
-                  </div>
-                </div>
-              </div>
-              <div class="item">
-                <div class="block-4 text-center">
-                  <figure class="block-4-image">
-                    <img src="images/cloth_3.jpg" alt="Image placeholder" class="img-fluid">
-                  </figure>
-                  <div class="block-4-text p-4">
-                    <h3><a href="#">T-Shirt Mockup</a></h3>
-                    <p class="mb-0">Finding perfect products</p>
-                    <p class="text-primary font-weight-bold">$50</p>
-                  </div>
-                </div>
-              </div>
-              <div class="item">
-                <div class="block-4 text-center">
-                  <figure class="block-4-image">
-                    <img src="images/shoe_1.jpg" alt="Image placeholder" class="img-fluid">
-                  </figure>
-                  <div class="block-4-text p-4">
-                    <h3><a href="#">Corater</a></h3>
-                    <p class="mb-0">Finding perfect products</p>
-                    <p class="text-primary font-weight-bold">$50</p>
-                  </div>
-                </div>
-              </div>
+              <?php endforeach; ?>
+              
+              
             </div>
           </div>
         </div>
@@ -259,8 +239,10 @@ $category = query("SELECT * FROM `category`");
           </div>
           <div class="col-md-12 col-lg-5 text-center pl-md-5">
             <h2><a href="#">50% less in all items</a></h2>
-            <p class="post-meta mb-4">By <a href="#">Carl Smith</a> <span class="block-8-sep">&bullet;</span> September 3, 2018</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam iste dolor accusantium facere corporis ipsum animi deleniti fugiat. Ex, veniam?</p>
+            <p class="post-meta mb-4">By <a href="#">Carl Smith</a> <span class="block-8-sep">&bullet;</span> September
+              3, 2018</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam iste dolor accusantium facere corporis
+              ipsum animi deleniti fugiat. Ex, veniam?</p>
             <p><a href="#" class="btn btn-primary btn-sm">Shop Now</a></p>
           </div>
         </div>
@@ -304,7 +286,7 @@ $category = query("SELECT * FROM `category`");
             <a href="#" class="block-6">
               <img src="images/hero_1.jpg" alt="Image placeholder" class="img-fluid rounded mb-4">
               <h3 class="font-weight-light  mb-0">Finding Your Perfect Shoes</h3>
-              <p>Promo from  nuary 15 &mdash; 25, 2019</p>
+              <p>Promo from nuary 15 &mdash; 25, 2019</p>
             </a>
           </div>
           <div class="col-md-6 col-lg-3">
@@ -331,12 +313,17 @@ $category = query("SELECT * FROM `category`");
         <div class="row pt-5 mt-5 text-center">
           <div class="col-md-12">
             <p>
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            Copyright &copy;<script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" class="text-primary">Colorlib</a>
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+              Copyright &copy;
+              <script data-cfasync="false"
+                src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+              <script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made
+              with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank"
+                class="text-primary">Colorlib</a>
+              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
             </p>
           </div>
-          
+
         </div>
       </div>
     </footer>
@@ -351,6 +338,7 @@ $category = query("SELECT * FROM `category`");
   <script src="js/aos.js"></script>
 
   <script src="js/main.js"></script>
-    
-  </body>
+
+</body>
+
 </html>
