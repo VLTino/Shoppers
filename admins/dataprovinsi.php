91ff337ed0e85c3904b6a115011fc97e83f8568c
VLTino <valent14206@gmail.com>
2023-07-12 20:49:15 +0700

    raja ongkir db

--- admins/dataprovinsi.php
+++ admins/dataprovinsi.php
@@ -1,36 +1,15 @@
 <?php
 
-$curl = curl_init();
+require 'rajaongkirdb.php';
 
-curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://api.rajaongkir.com/starter/province",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "GET",
-  CURLOPT_HTTPHEADER => array(
-    "key: f39965e7eae6f0ebbcbea6c800553c08"
-  ),
-));
-
-$response = curl_exec($curl);
-$err = curl_error($curl);
-
-curl_close($curl);
-
-if ($err) {
-    echo "cURL Error #:" . $err;
-  } else {
-    $array_response = json_decode($response,TRUE);
-    $dataprovinsi = $array_response["rajaongkir"]["results"];
+$dataprovinsi = query("SELECT * FROM `tb_ro_provinces`");
     
     echo "<option> Pilih Provinsi </option>";
 
     foreach ($dataprovinsi as $key => $prov) {
       echo "<option value='".$prov["province_id"]."' id_provinsi='".$prov["province_id"]."'>";
-      echo $prov["province"];
+      echo $prov["province_name"];
       echo "</option>";
     }
-  }
+  
+  ?>
